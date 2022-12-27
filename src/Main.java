0f9169d06e76c404148a209a73357678259c3342
aufall02 <aufaluzumaki@gmail.com>
2022-12-27 16:46:40 +0700

    pohon ga dadi

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 
 
-import objects.Pesawat;
+import objects.*;
 
 
 
@@ -14,12 +14,18 @@ public class Main extends JFrame {
     public void paint(Graphics g){
         Graphics2D g2d = (Graphics2D) g;
         Pesawat miring = new Pesawat();
+        Pohon p = new Pohon();
+
+
+
+
         g2d.draw(miring);
+        g2d.draw(p);
 
     }
     public static void main(String[] args) {
         Main test = new Main();
-        test.setSize(1920,1080);
+        test.setSize(1366,768);
         test.setLocationRelativeTo(null);
         test.setVisible(true);
     }
