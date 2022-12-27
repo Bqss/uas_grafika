fba906cc0bb39e0ce2c4ce310eecd36142844e83
aufall02 <aufaluzumaki@gmail.com>
2022-12-27 15:02:13 +0700

    coba anjir

--- src/objects/Pohon.java
+++ src/objects/Pohon.java
@@ -1,5 +1,11 @@
 package objects;
-import java.awt.*;
-public class Pohon {
+import java.awt.geom.Line2D;
 
+public class Pohon {
+    public  Pohon(){
+        Line2D test = new Line2D.Double(500,500,300,300);
+
+    }
 }
+
+
